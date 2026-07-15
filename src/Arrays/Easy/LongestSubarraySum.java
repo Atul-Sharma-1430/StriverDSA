398882ec65b45333888f9970af6ad65d3280a1f8
Atul-Sharma-1430 <atulsharma000007@gmail.com>
2026-07-15 21:05:46 +0530

    Solved LongestSubarraySum question with a better approach using prefixSum and Hashmap

--- src/Arrays/Easy/LongestSubarraySum.java
+++ src/Arrays/Easy/LongestSubarraySum.java
@@ -1,5 +1,7 @@
 package Arrays.Easy;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class LongestSubarraySum {
@@ -65,6 +67,46 @@ public class LongestSubarraySum {
                 }
             }
 
+        }
+
+        return maxLen;
+    }
+
+    // TC : O(n)
+    // SC : O(n)
+    public static int better(int[] array, int target) {
+        int maxLen = 0;
+        long prefixSum = 0;// Carries the total sum till current index
+
+        // Map to Store the total sum at each index
+        HashMap<Long, Integer> myHashMap = new HashMap<>();
+
+        for (int i = 0; i < array.length; i++) {
+            prefixSum += array[i];
+
+            // we get sum == target means we got a subarray then update length accordingly
+            if (prefixSum == target) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+
+            // prev to store the value which is the difference of current sum and target
+            long prev = prefixSum - target;
+
+            // If map has that prev value then we find the length of current subarray from that point to current index and then adjust length accordingly
+            if (myHashMap.containsKey(prev)) {
+                int tempLen = i - myHashMap.get(prev);
+                maxLen = Math.max(maxLen, tempLen);
+            }
+
+            // Store only the first occurrence of the prefix sum.
+            // Example: array = [2, 0, 0, 3], target = 3
+            // Prefix sum 2 appears at indices 0, 1, and 2.
+            // Keep index 0 because it gives the longest subarray [0, 0, 3] (length 3).
+            // Hence we will just track its first occurance only where we get the prev
+            // It basically Handles zero
+            if (!myHashMap.containsKey(prefixSum)) {
+                myHashMap.put(prefixSum, i);
+            }
         }
 
         return maxLen;
