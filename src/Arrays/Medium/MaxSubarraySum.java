27eef4d985b749ebab282d24685a8227ae04f7fc
Atul-Sharma-1430 <atulsharma000007@gmail.com>
2026-07-22 01:06:11 +0530

    Added Extended question of Maximum sub array sum where it is asked to return the subarray whose sum is maximum

--- src/Arrays/Medium/MaxSubarraySum.java
+++ src/Arrays/Medium/MaxSubarraySum.java
@@ -1,6 +1,7 @@
 package Arrays.Medium;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 class MaxSubarraySum {
     public static void main(String[] args) {
@@ -16,7 +17,7 @@ class MaxSubarraySum {
             array[i] = sc.nextInt();
         }
 
-        System.out.println(KadaneAlgo(array));
+        System.out.println(Arrays.toString(maxSubarray(array)));
 
         sc.close();
     }
@@ -61,5 +62,61 @@ class MaxSubarraySum {
         }
 
         return maxSum;
+    }
+
+    // If it is asked to return the subarray with maximum sum,
+    // then along with maxSum we also need to track the start and end indexes.
+    public static int[] maxSubarray(int[] array) {
+
+        int sum = 0;
+
+        // Stores the start index of the final answer subarray.
+        int stIndex = -1;
+
+        // Stores the end index of the final answer subarray.
+        int endIndex = -1;
+
+        // Tracks the starting index of the CURRENT subarray.
+        // Isko directly stIndex nahi bana sakte.
+        // Kyuki ho sakta hai current subarray baad me reject ho jaye.
+        // Jab bhi naya maximum sum milega tabhi st ki value stIndex me save hogi.
+        int st = 0;
+
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+
+            sum += array[i];
+
+            // Agar current sum ne naya maximum banaya,
+            // to current subarray hi answer ban jayegi.
+            // Isliye uske start aur end indexes save kar do.
+            if (sum > maxSum) {
+                maxSum = sum;
+                stIndex = st;
+                endIndex = i;
+            }
+
+            // Agar sum negative ho gaya,
+            // to iske aage continue karne ka koi fayda nahi.
+            // Next subarray i+1 se start hogi.
+            if (sum < 0) {
+                sum = 0;
+                st = i + 1;
+            }
+        }
+
+        System.out.println(maxSum);
+
+        int[] ansArray = new int[endIndex - stIndex + 1];
+
+        int index = 0;
+
+        // Copy the maximum sum subarray into the answer array.
+        for (int i = stIndex; i <= endIndex; i++) {
+            ansArray[index++] = array[i];
+        }
+
+        return ansArray;
     }
 }
