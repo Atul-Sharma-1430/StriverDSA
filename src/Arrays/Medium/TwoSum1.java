f556d692415ea9d32ff4ffbcfc8bcdcbac41828c
Atul-Sharma-1430 <atulsharma000007@gmail.com>
2026-07-24 21:27:48 +0530

    closed the scanner class

--- src/Arrays/Medium/TwoSum1.java
+++ src/Arrays/Medium/TwoSum1.java
@@ -18,9 +18,11 @@ public class TwoSum1 {
         }
 
         System.out.print("Enter the Target value: ");
-        int target  = sc.nextInt();
+        int target = sc.nextInt();
 
         System.out.println((Arrays.toString(twoSumOptimal(array, target))));
+
+        sc.close();
     }
 
     // TC : O(n2)
@@ -28,35 +30,35 @@ public class TwoSum1 {
     public static int[] brute(int[] array, int target) {
 
         for (int i = 0; i < array.length; i++) {
-            for (int j = i+1; j < array.length ; j++) {
-                if(array[i] + array[j] == target) {
-                    return new int[] {i , j};
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] + array[j] == target) {
+                    return new int[] { i, j };
                 }
             }
         }
 
-        return new int[] {-1, -1};
+        return new int[] { -1, -1 };
     }
-
 
     // TC : O(n)
     // SC : O(n)
     public static int[] twoSumOptimal(int[] array, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i=0; i<array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             int rem = target - array[i];
 
-            // if the map has already that remaining value means we got the pair so return it
+            // if the map has already that remaining value means we got the pair so return
+            // it
             if (map.containsKey(rem)) {
                 int j = map.get(rem);
-                return new int[] {j,i};
+                return new int[] { j, i };
             }
 
             // else store tne value in map
             map.put(array[i], i);
         }
 
-        return new int[] {-1, -1};
+        return new int[] { -1, -1 };
     }
 }
