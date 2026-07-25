504baedbea2e22762e70f8f566a20859218302c5
Atul-Sharma-1430 <atulsharma000007@gmail.com>
2026-07-25 22:32:50 +0530

    Solved Set matrix zero with brute force approach

---
+++ src/Arrays/Medium/SetMatrixZero.java
@@ -0,0 +1,73 @@
+package Arrays.Medium;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class SetMatrixZero {
+    // TC : O(N3)
+    // SC : O(1)
+    public static void setZerosBrute(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                // If val is zero
+                if (matrix[i][j] == 0) {
+                    // mark entire row -1 where val = 1
+                    for (int k = 0; k < matrix[i].length; k++) {
+                        if (matrix[i][k] != 0) {
+                            matrix[i][k] = -1;
+                        }
+                    }
+
+                    // make entire col -1 where val = 1
+                    for (int k = 0; k < matrix.length; k++) {
+                        if (matrix[k][j] != 0) {
+                            matrix[k][j] = -1;
+                        }
+                    }
+                }
+            }
+        }
+
+        // replace all places of -1 with 0
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == -1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter matrix elements:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        setZerosBrute(matrix);
+        System.out.println("Matrix after setting zeroes:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
