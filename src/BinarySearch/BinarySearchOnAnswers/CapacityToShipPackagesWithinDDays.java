f0405b6bd9c82dad2adef721d6e1b5051de2787e
Atul-Sharma-1430 <atulsharma000007@gmail.com>
2026-08-15 21:34:24 +0530

    Added the pattern which is used to solve this problem

--- src/BinarySearch/BinarySearchOnAnswers/CapacityToShipPackagesWithinDDays.java
+++ src/BinarySearch/BinarySearchOnAnswers/CapacityToShipPackagesWithinDDays.java
@@ -2,6 +2,7 @@ package BinarySearch.BinarySearchOnAnswers;
 
 import java.util.Scanner;
 
+// Pattern : Binary search on Answers
 public class CapacityToShipPackagesWithinDDays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
