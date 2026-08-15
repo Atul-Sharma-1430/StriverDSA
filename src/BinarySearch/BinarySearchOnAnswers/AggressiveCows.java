f51a6e67196b4430715fcc6155193795f288b984
Atul-Sharma-1430 <atulsharma000007@gmail.com>
2026-08-15 22:56:27 +0530

    Added the pattern steps which have to do for these kind of problems

--- src/BinarySearch/BinarySearchOnAnswers/AggressiveCows.java
+++ src/BinarySearch/BinarySearchOnAnswers/AggressiveCows.java
@@ -87,3 +87,11 @@ public class AggressiveCows {
         return ans;
     }
 }
+
+// 1. Answer ka search space/range dhundo
+//          ↓
+// 2. Mid mein ek possible answer choose karo
+//          ↓
+// 3. Check karo: "Kya ye answer possible hai?"
+//        ↓
+// 4. Possible / Not Possible ke basis par pointer move karo
