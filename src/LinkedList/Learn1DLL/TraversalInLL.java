f0cf6f52c115ba1b58491cc49a97b43d56fb1aff
Atul-Sharma-1430 <atulsharma000007@gmail.com>
2026-09-13 13:37:02 +0530

    Learned how to traverse in LL

---
+++ src/LinkedList/Learn1DLL/TraversalInLL.java
@@ -0,0 +1,47 @@
+package LinkedList.Learn1DLL;
+
+public class TraversalInLL {
+
+    public static Node convertArrayToLL(int[] arr) {
+
+        // Pahle element ko head bana denge
+        Node head = new Node(arr[0]);
+
+        // Mover node ko head par rakhenge jo aage nodes ko link karega
+        Node mover = head;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            // Current element ke liye ek new node banayenge
+            Node temp = new Node(arr[i]);
+
+            // Mover ke next mein temp ko daal kar nodes ko link kar denge
+            mover.next = temp;
+
+            // Mover ko temp par shift kar denge
+            mover = temp;
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {3, 6, 7, 8, 3, 4, 5, 7};
+
+        Node head = convertArrayToLL(arr);
+
+        // Traverse karne ke liye temp node banayenge kyuki head ko move nahi karna hai
+        Node temp = head;
+
+        // Jab tak last node tak nahi pahunchte
+        while (temp != null) {
+
+            // Current node ka data print karenge
+            System.out.print(temp.data + " ");
+
+            // Next node par move karenge
+            temp = temp.next;
+        }
+    }
+}
