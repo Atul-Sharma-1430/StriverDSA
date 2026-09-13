f36f93dfc249a907162c051bf4b9c1507cefacc7
Atul-Sharma-1430 <atulsharma000007@gmail.com>
2026-09-13 13:21:17 +0530

    Moved Node template code to a seperate file

---
+++ src/LinkedList/Learn1DLL/Node.java
@@ -0,0 +1,17 @@
+package LinkedList.Learn1DLL;
+
+// Linked List Template
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
